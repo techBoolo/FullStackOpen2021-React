8d96c606d694c82a2682cbbfda57165a03637430
techboolo <teklufa@gmail.com>
2021-06-28 18:01:41 +0300

    components may contain any valid javascript code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
 function App() {
+  console.log("Components can contain any valid javascript code");
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+
   return (
     <div className="App">
-      <p>Hello world</p>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>{a} plus {b} equals {a + b}</p>
     </div>
   );
 }
