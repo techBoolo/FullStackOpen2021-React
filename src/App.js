58b69889e6b5063989abc657d74276d615dcfce3
techboolo <teklufa@gmail.com>
2021-06-28 18:39:34 +0300

    passing data to components with prop

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import './App.css';
 
-const Hello = () => {
+const Hello = (props) => {
   return (
       <div>
-        <p>Hello world</p>
+        <p>Hello { props.name }</p>
       </div>
     )
 }
 
+const Hello2 = (props) => {
+  return (
+      <div>
+        <p>Hello { props.name } you are { props.age } years old.</p>
+      </div>
+    )
+}
 const  App = () =>  {
 
   return (
       <div className='App'>
         <h1>Greeting</h1>
-        <Hello />
-        <Hello />
+        <Hello name='Geo'/>
+        <Hello name='Daisy'/>
+        <Hello2 name='Will' age='34' />
       </div>
     )
 }
