e8db3623c93ed49212cd78376c80c0c1c1a9c1c3
techboolo <teklufa@gmail.com>
2021-07-05 16:29:14 +0300

    add new note with form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Note from './components/Note';
 
 import './App.css';
 
-const notes = [
+const notesData = [
   {
     id: 1,
     content: "Html",
@@ -25,9 +25,37 @@ const notes = [
 ]
 
 const  App = () =>  {
+  const [ notes, setNotes ] = useState(notesData);
+  const [ content, setContent ] = useState('');
+
+  const generateId = (notes) => {
+    const maxId = Math.max(...notes.map(note => note.id));
+    return maxId + 1;
+  }
+
+  const addNote = (ev) => {
+    ev.preventDefault();
+    const newNote = {
+      content,
+      id: generateId(notes),
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+    }
+    setNotes([...notes, newNote]);
+    setContent('');
+    console.log(newNote);
+  }
+
+  const handleContentChange = (ev) => {
+    setContent(ev.target.value);
+  }
 
   return (
       <div className='App'>
+        <form onSubmit={addNote}>
+          <input value={content} onChange={handleContentChange} />
+          <button type='submit'>add</button>
+        </form>
         <h4>Notes</h4>
         <ul>
           { notes.map(note =>  <Note key={note.id} note={note} />) }
