d1e41a5398bb896a747510a611c2b428128f08f5
techboolo <teklufa@gmail.com>
2021-07-05 13:46:37 +0300

    ex 2.1 course information step6

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
+import Course from './components/Course';
 import './App.css';
 
 function App() {
   const course = {
+    id: 1,
     name: 'Half stack application development',
     parts: [
-      { name: 'Fundamental of React', exercises: 10 },
-      { name:  'using proops to pass data', exercises:  7 },
-      { name:  'state of a component', exercises: 14 }
+      { name: 'Fundamental of React', exercises: 10, id: 1 },
+      { name:  'using proops to pass data', exercises:  7, id: 2 },
+      { name:  'state of a component', exercises: 14, id: 3 }
     ]
-  }
-
-  const Header = (props) => {
-    return  <h1>{ props.course }</h1>
-  }
-  const Content = (props) => {
-    return (
-        <div>
-          <Part part={props.parts[0]} />
-          <Part part={props.parts[1]} />
-          <Part part={props.parts[2]} />
-        </div>
-        ) 
-  }
-  const Total = (props) => {
-    const { parts } = props
-    return <p>Number of exercises { parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-  }
-  const Part = (props) => {
-    const { part } = props
-    return <p>{ part.name } { part.exercise }</p>
   }
 
   return (
     <div className="App">
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Course course={course} />
     </div>
   );
 }
