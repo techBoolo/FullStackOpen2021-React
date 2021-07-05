8d954e9049426436358c27595a2a71522bb83cfc
techboolo <teklufa@gmail.com>
2021-07-05 14:02:23 +0300

    ex 2.4 course information, step9

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import Course from './components/Course';
 import './App.css';
 
 function App() {
-  const course = {
+  const courses = [
+    {
     id: 1,
     name: 'Half stack application development',
     parts: [
-      { name: 'Fundamental of React', exercises: 10, id: 1 },
-      { name:  'using proops to pass data', exercises:  7, id: 2 },
-      { name:  'state of a component', exercises: 14, id: 3 }
+        { name: 'Fundamental of React', exercises: 10, id: 1 },
+        { name:  'using proops to pass data', exercises:  7, id: 2 },
+        { name:  'state of a component', exercises: 14, id: 3 }
+      ]
+    },
+    {
+    id: 2,
+    name: 'Node.js',
+    parts: [
+      { name: 'Routing', exercises: 3, id: 1 },
+      { name:  'Middlewares', exercises:  7, id: 2 }
     ]
   }
 
+  ]
   return (
     <div className="App">
-      <Course course={course} />
+      { courses.map(course => (
+        <Course key={course.id} course={course} />
+      )) } 
     </div>
   );
 }
