26d5b803a7e5845371267f726fcdc939f6f69942
techboolo <teklufa@gmail.com>
2021-07-05 13:12:33 +0300

    part 2a, rendering collections

--- src/App.js
+++ src/App.js
@@ -1,44 +1,36 @@
-import React, { useState } from 'react';
-import Display from './components/Display';
-import Button from './components/Button';
-import History from './components/History';
+import React from 'react';
 
 import './App.css';
 
+const notes = [
+  {
+    id: 1,
+    content: "Html",
+    date: new Date(),
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser",
+    date: new Date(),
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST",
+    date: new Date(),
+    important: true
+  }
+]
+
 const  App = () =>  {
-  const [ clicks, setClicks ] = useState({ left: 0, right: 0 });
-  const [ allClicks, setAll ] = useState([]);
-  const [value, setValue ] = useState(0);
-
-  const handleLeftClick = () => {
-    setClicks({...clicks, left: clicks.left + 1 });
-    setAll([...allClicks, 'L'])
-  }
-  const handleRightClick = () => {
-    setClicks(prevRight => ({ ...clicks, right: prevRight.right + 1}))
-    setAll([...allClicks, 'R'])
-  }
-
-  const setToValue = (newValue) => setValue(newValue)
 
   return (
       <div className='App'>
-        <Display value={clicks.left} />
-        <Button 
-          handleClick={handleLeftClick} 
-          text='Left' 
-        />
-        <Button handleClick={handleRightClick} text='Right' />
-        <Display value={clicks.right} />
-        <div>
-          <History allClicks={allClicks} />
-        </div>
-        <div>
-          { value }
-          <Button handleClick={() => setToValue(1000)} text='Thousand' />
-          <Button handleClick={() => setToValue(0)} text='Reset' />
-          <Button handleClick={() => setToValue(value + 1)} text='Inc' />
-        </div>
+        <h4>Notes</h4>
+        <ul>
+          { notes.map(note =>  <li key={note.id}>{ note.content }</li>) }
+        </ul>
       </div>
     )
 }
