b9623baa47b284a7c3310cf7e2de5a58087ecaa4
techboolo <teklufa@gmail.com>
2021-07-06 13:35:53 +0300

    ex 2.9 phonebook, step4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [ people, setPeople ] = useState([ { name: "Arto Hellas", number: '040-123456' } ])
+  const [ people, setPeople ] = useState([ 
+      { name: "Arto Hellas", number: '040-123456' },
+      { name: "ada Lovelace", number: "39-44-5323523" },
+      { name: "Dan Abramov", number: "12-34-5678" },
+      { name: "Mary Poppend", number: "39-23-64356" }
+  ])
   const [ name, setName ] = useState('');
   const [ number, setNumber ] = useState('');
+  const [ searchTerm, setSearchTerm ] = useState('');
+  const [ filteredPeople, setFilteredPeople ] = useState([]);
 
   const processName = (name) => {
     return name.split(/\s+/).join(' ').trim();
@@ -27,10 +34,20 @@ function App() {
     setNumber(ev.target.value); 
   }
 
+  const searchPeople = (ev) => {
+    setSearchTerm(ev.target.value);
+  }
+  
+  useEffect(() => {
+    setFilteredPeople(people.filter(person => processName(person.name).toLowerCase().includes(processName(searchTerm).toLowerCase()))) 
+  }, [searchTerm, people])
+
   return (
     <div className="App">
       <h4>Pb</h4>
       <form onSubmit={handleSubmit}>
+        S: <input value={searchTerm} onChange={searchPeople} />
+        <h4>Add new</h4>
         N: <input value={name} onChange={handleNameChange} /><br />
         Nu: <input value={number} onChange={handleNumberChange} /><br />
         <button type='submit'>add</button>
@@ -38,7 +55,7 @@ function App() {
       <h4>Nm</h4>
       <ul>
         {
-          people.map(person => <li key={person.name}>{ person.name } {person.number}</li>)
+          filteredPeople.map(person => <li key={person.name}>{ person.name } {person.number}</li>)
         }
       </ul>
     </div>
