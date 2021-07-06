f389e76a698628eab0ffd10988c5b4503ed3ce9c
techboolo <teklufa@gmail.com>
2021-07-06 11:08:31 +0300

    part 2b, ex 2.6 phonebook, step1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ people, setPeople ] = useState([ { name: "Arto Hellas" } ])
+  const [ name, setName ] = useState('');
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    setPeople([...people, { name }])
+    setName('');
+  }
+  const handleNameChange = (ev) => {
+    setName(ev.target.value); 
+  }
+
   return (
     <div className="App">
-      <p>Hello world</p>
+      <h4>Phonebook</h4>
+      <form onSubmit={handleSubmit}>
+        <input value={name} onChange={handleNameChange} />
+        <button type='submit'>add</button>
+      </form>
+      <h4>Numbers</h4>
+      <ul>
+        {
+          people.map(person => <li key={person.name}>{ person.name }</li>)
+        }
+      </ul>
     </div>
   );
 }
