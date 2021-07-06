1fb7bbe8652ca77176bfbf3ffb176518982edf07
techboolo <teklufa@gmail.com>
2021-07-06 10:01:44 +0300

    ex 1.13 anecdotes, step2

--- src/App.js
+++ src/App.js
@@ -11,15 +11,27 @@ function App() {
     'Debugging is twice as hard as writing the code',
     'programming without an extremely heavy use of console.log'
   ]
+  const anecdotesLength = anecdotes.length;
+  const arr = [...Array(anecdotesLength).keys()];
+  const initVotes = {};
+  arr.map(k => initVotes[k] = 0)
+  
   const [ selected, setSelected] = useState(0);
+  const [ votes, setVotes] = useState(initVotes);
+
   const selectAnecdote = () => {
-    const num = Math.trunc(Math.random() * anecdotes.length);
+    const num = Math.trunc(Math.random() * anecdotesLength);
     setSelected(num);
   }
 
+  const handleVote = () => {
+    setVotes({...votes, [selected]: votes[selected] + 1})
+  }
   return (
     <div className="App">
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={handleVote}>vote</button>
       <button onClick={selectAnecdote}>next</button>
     </div>
   );
