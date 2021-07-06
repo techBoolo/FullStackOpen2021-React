b15cd314d916ce9a81203e85d671a134fca562d2
techboolo <teklufa@gmail.com>
2021-07-06 09:15:03 +0300

    ex 1.12 anecdotes, step1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const anecdotes = [
+    'if it hurts, do it more often',
+    'adding number to a late project makes it later',
+    'The first 90 percent of the code accounts for the first 90 percent of the dev time',
+    'Any fool can write code',
+    'premature optimization is the root of all evil',
+    'Debugging is twice as hard as writing the code',
+    'programming without an extremely heavy use of console.log'
+  ]
+  const [ selected, setSelected] = useState(0);
+  const selectAnecdote = () => {
+    const num = Math.trunc(Math.random() * anecdotes.length);
+    setSelected(num);
+  }
+
   return (
     <div className="App">
-      <p>Hello world</p>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={selectAnecdote}>next</button>
     </div>
   );
 }
