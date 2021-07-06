9d7a361f9d5b8f330fd8e6a882ed6c2dad3eaa0e
techboolo <teklufa@gmail.com>
2021-07-06 12:51:42 +0300

    ex 2.7 phonebook, step2

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ function App() {
   const [ people, setPeople ] = useState([ { name: "Arto Hellas" } ])
   const [ name, setName ] = useState('');
 
+  const processName = (name) => {
+    return name.split(/\s+/).join(' ').trim();
+  }
   const handleSubmit = (ev) => {
     ev.preventDefault();
-    setPeople([...people, { name }])
-    setName('');
+    if(people.find(person => processName(person.name).toLowerCase() === processName(name).toLowerCase())){
+      alert(`${name} is already added to phonebook`)
+      setName('');
+    }else{ 
+      setPeople([...people, { name: processName(name) }])
+      setName('');
+    }
   }
   const handleNameChange = (ev) => {
     setName(ev.target.value); 
@@ -16,12 +24,12 @@ function App() {
 
   return (
     <div className="App">
-      <h4>Phonebook</h4>
+      <h4>Pb</h4>
       <form onSubmit={handleSubmit}>
         <input value={name} onChange={handleNameChange} />
         <button type='submit'>add</button>
       </form>
-      <h4>Numbers</h4>
+      <h4>Nm</h4>
       <ul>
         {
           people.map(person => <li key={person.name}>{ person.name }</li>)
