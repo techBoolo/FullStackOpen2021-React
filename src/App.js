4790b84ae463c78b3c1c20f22ab0d6faec8d124a
techboolo <teklufa@gmail.com>
2021-07-06 13:00:16 +0300

    ex 2.8 phonebook, step3

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [ people, setPeople ] = useState([ { name: "Arto Hellas" } ])
+  const [ people, setPeople ] = useState([ { name: "Arto Hellas", number: '040-123456' } ])
   const [ name, setName ] = useState('');
+  const [ number, setNumber ] = useState('');
 
   const processName = (name) => {
     return name.split(/\s+/).join(' ').trim();
@@ -14,25 +15,30 @@ function App() {
       alert(`${name} is already added to phonebook`)
       setName('');
     }else{ 
-      setPeople([...people, { name: processName(name) }])
+      setPeople([...people, { name: processName(name), number }])
       setName('');
+      setNumber('');
     }
   }
   const handleNameChange = (ev) => {
     setName(ev.target.value); 
+  }
+  const handleNumberChange = (ev) => {
+    setNumber(ev.target.value); 
   }
 
   return (
     <div className="App">
       <h4>Pb</h4>
       <form onSubmit={handleSubmit}>
-        <input value={name} onChange={handleNameChange} />
+        N: <input value={name} onChange={handleNameChange} /><br />
+        Nu: <input value={number} onChange={handleNumberChange} /><br />
         <button type='submit'>add</button>
       </form>
       <h4>Nm</h4>
       <ul>
         {
-          people.map(person => <li key={person.name}>{ person.name }</li>)
+          people.map(person => <li key={person.name}>{ person.name } {person.number}</li>)
         }
       </ul>
     </div>
